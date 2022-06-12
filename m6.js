68fbe7459976c79b5a1a372d5474cbf7aa90fcb4
Abdul Rahman <abdulrahmanasus@outlook.com>
2022-06-12 20:09:45 +0530

    changes 12/6/2022

---
+++ m6.js
@@ -0,0 +1,11 @@
+const {readFile}=require('fs')
+console.log('started a first task')
+readFile('./new.txt','utf8',(err,result)=>{
+    if (err){
+        console.log(err)
+        return 
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+console.log('starting next task')
